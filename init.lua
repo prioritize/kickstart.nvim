6d551a8c3b2a5a094fa081bb8194244ea0b7a4ba
prioritize <jared@prototypic.al>
2024-07-24 12:51:54 -0700

    autopairs and rustaceanvim added

--- init.lua
+++ init.lua
@@ -249,6 +249,18 @@ require('lazy').setup({
     },
   },
   {
+    'windwp/nvim-autopairs',
+    event = 'InsertEnter',
+    config = true,
+    -- use opts = {} for passing setup options
+    -- this is equalent to setup({}) function
+  },
+  {
+    'mrcjkb/rustaceanvim',
+    version = '^4', -- Recommended
+    lazy = false, -- This plugin is already lazy
+  },
+  {
     'akinsho/toggleterm.nvim',
     version = '*',
     config = true,
