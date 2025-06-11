e9fe405163591b4efd997f3b3e573caa05051561
prioritize <jared@prototypic.al>
2025-06-10 17:55:10 -0700

    progress for v0.11

--- init.lua
+++ init.lua
@@ -198,6 +198,9 @@ vim.keymap.set('n', '<C-k>', '<cmd> TmuxNavigateUp<CR>', { desc = 'Move focus to
 -- vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
 -- vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 -- vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+vim.diagnostic.config {
+  virtual_lines = true,
+}
 
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
